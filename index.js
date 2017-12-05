0ded25e442d1ab9ce35cadb8eacf0dfb287ea626
coomeva <34242282+coomeva@users.noreply.github.com>
2017-12-05 08:35:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ serviceRest.use(bodyParser.urlencoded({ extended: true }));
 serviceRest.use(bodyParser.json());
 
 serviceRest.post('/RestHome', function(req, res) {
-
+    var data = 'Consume rest services'
+    
+    return res.json({
+      speech: data,
+      displayText: data,
+      source: 'rest-for-googlehome'
+    });
 });
 
 serviceRest.listen((process.env.PORT || 4040), function() {
